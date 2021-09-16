dd9236f926127afaabcc4e849160307310f74cc8
muthu <mkandanmk7@gmail.com>
2021-09-16 21:35:31 +0530

    nav done

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,34 +1,30 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
 import { FaReact } from "react-icons/fa";
+// import { IconName } from "react-icons/md";
 
 export default function Header() {
   return (
-    <nav className="navbar">
-      <div className="container-fluid">
-        <span className="navbar-brand">
+    <>
+      <nav className="navbar">
+        <span>
           <Link to="/">
-            <FaReact style={{ width: "40px", height: "40px" }} />
+            <FaReact style={{ height: "50px", width: "50px" }} />
           </Link>
         </span>
-        <ul className="nav justify-content-end">
-          <li className="nav-item">
-            <NavLink activeClassName="active" className="nav-link" to="/home">
-              Home
-            </NavLink>
+
+        <ul className="nav ">
+          <li>
+            <NavLink to="/home">Home</NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink activeClassName="active" className="nav-link" to="/posts">
-              Posts
-            </NavLink>
+          <li>
+            <NavLink to="/posts">Posts</NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink activeClassName="active" className="nav-link" to="/about">
-              About
-            </NavLink>
+          <li>
+            <NavLink to="/about">About</NavLink>
           </li>
         </ul>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
